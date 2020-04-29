a07f18e2fcddcdd83ea77f72f3c7dcea5ce88187
k4mber <63923733+k4mber@users.noreply.github.com>
2020-04-29 10:13:38 -0400

    Update moviesForRent.php
    Added a separate query to create the view, TODO: Make separate queries for each store using a dropdown.

--- moviesForRent.php
+++ moviesForRent.php
@@ -1,4 +1,45 @@
-<html>
-  <title>Rent a Movie!</title>
-  <body><p>Do it.</p></body>
-</html>
+<?php
+include("phpbook-vars.inc");
+
+include("displayDBQueryField.inc");
+
+$database="Team1";
+
+$connect=mysqli_connect($hostname, $user, $password);
+
+mysqli_select_db($connect, $database);
+?>
+<Html>
+<Head>
+<Title>Rent a Movie!</Title>
+</Head>
+<Body>
+
+<Table Border=0 cellPadding=10 Width=100%>
+
+<Tr>
+
+<Td BGColor="F0F8FF" Align=Center VAlign=top Width=17%> </Td>
+
+<Td BGColor="FFFFFF" Align=Left VAlign=Top Width=83%>
+
+<?php
+//$movie_name = $_POST['movie'];
+$query0='Create or Replace View Rentable 
+as select M.Name 
+from Movies M,Stores S 
+where M.SID=S.SID and M.SID=S.SID and S.Name="Kaer Morhen";';
+$query1="SELECT * FROM Rentable;";
+echo "<h1>The following displays the stock of plymouth store depending on which store is selected.</h1>";
+echo "<br>";
+mysqli_query($connect, $query0);
+display_db_query($query1, $connect,True,2);
+
+mysqli_close($connect);
+?>
+</Td>
+
+</Tr>
+</Table>
+</Body>
+</Html>
